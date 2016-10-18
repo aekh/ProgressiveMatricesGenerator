068c16fee9e8c617bd3506976509f2b5f3d47a89
Gojs <djalekzse@gmail.com>
2016-10-19 00:38:39 +0200

    Simplified operations

---
+++ src/matrix/Axis.java
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+package matrix;
+
+/**
+ * @author aekh
+ *
+ */
+enum Axis { // Orientation, angle etc. We need north west east and omni etc.
+	Horizontal,
+	Vertical,
+	Diagonal,
+	Antidiagonal
+}
---
+++ src/matrix/Revolution.java
@@ -0,0 +1,10 @@
+package matrix;
+
+/**
+ * Created by Gojs on 2016-07-13.
+ */
+public enum Revolution {
+    Quarter,
+    Half,
+    Three
+}
